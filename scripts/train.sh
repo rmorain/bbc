2ffe1a355a3abd5e123f6c4d9a8315221ea506e1
Robert Morain <robert.morain@gmail.com>
2024-07-01 16:29:49 -0600

    Increased memory

--- scripts/train.sh
+++ scripts/train.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks=8   # number of processor cores (i.e. tasks)
 #SBATCH --nodes=1   # number of nodes
 #SBATCH --gpus=8
-#SBATCH --mem-per-cpu=8G   # memory per CPU core
+#SBATCH --mem-per-cpu=64G   # memory per CPU core
 #SBATCH -J "Test train"   # job name
 #SBATCH --mail-user=rmorain2@byu.edu   # email address
 #SBATCH --qos=cs
